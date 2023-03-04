f4173144229be993af3c32b0aeadb6897d623b65
Ariel-Echeverria <121610016+Ariel-Echeverria@users.noreply.github.com>
2023-03-04 13:35:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function abrirWhatsApp() {
+  window.open("https://wa.me/+5491141715114", "_blank");
+}
+
+function abrirComerce() {
+  window.open("https://www.mercadolibre.com.ar/placa-de-video-amd-gigabyte-gaming-radeon-rx-500-series-rx-580-gv-rx580gaming-8gd-rev20-8gb/p/MLA15907843?matt_tool=74941839&matt_word=&matt_source=google&matt_campaign_id=14508409409&matt_ad_group_id=142443696844&matt_match_type=&matt_network=g&matt_device=c&matt_creative=619438930999&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=140252511&matt_product_id=MLA15907843-product&matt_product_partition_id=1994760955327&matt_target_id=aud-1930507555360:pla-1994760955327&gclid=Cj0KCQiAi8KfBhCuARIsADp-A57uK0_hzH-K8YpRPdG0BVH8fQwOaJ-3oZLIyKd-xOynn67KS-bVeeIaAmcbEALw_wcB", "_blank")
+}
+
+function abrirComerceNvidia() {
+  window.open("https://www.mercadolibre.com.ar/placa-de-video-nvidia-msi-geforce-10-series-gt-1030-geforce-gt-1030-2gd4-lp-oc-oc-edition-2gb/p/MLA15200025?matt_tool=74941839&matt_word=&matt_source=google&matt_campaign_id=14508409409&matt_ad_group_id=142443696844&matt_match_type=&matt_network=g&matt_device=c&matt_creative=619438930999&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=547137037&matt_product_id=MLA15200025-product&matt_product_partition_id=1994760955327&matt_target_id=aud-1930507555360:pla-1994760955327&gclid=Cj0KCQiAi8KfBhCuARIsADp-A55bP6XiWOfaEhCT72aJBqqHBkbK0FHwHCgpBVJaRKVCPp8RCU-dg58aAg8FEALw_wcB", "_blank")
+}
+
+function abrirComercePantalla() {
+  window.open("https://articulo.mercadolibre.com.ar/MLA-1341059219-monitor-gamer-led-24-fullhd-ips-75hz-samsung-lf24t350fhlczb-_JM?matt_tool=12492076&matt_word=&matt_source=google&matt_campaign_id=19580718235&matt_ad_group_id=149208093590&matt_match_type=&matt_network=g&matt_device=c&matt_creative=645605900839&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=249459138&matt_product_id=MLA1341059219&matt_product_partition_id=1939864541246&matt_target_id=aud-1930507555360:pla-1939864541246&gclid=Cj0KCQiAi8KfBhCuARIsADp-A57vHlahS3P17GkCF05MktATqsgys2Z-Jt_ZyuIPe-pdtbMGlanFDy0aAmP7EALw_wcB", "_blank")
+}
+
